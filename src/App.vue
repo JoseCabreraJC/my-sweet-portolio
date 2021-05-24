bc435082903a9e7d950996520ad44019b4219f05
JoseCabreraJC <josem.cabrera259@gmail.com>
2021-05-24 10:31:15 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <MyName />
+  <MyName />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import MyName from "./components/MyName.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    HelloWorld,
+    MyName,
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+}
+html {
+  scroll-behavior: smooth;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
---
+++ src/components/MyName.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="container">
+    <h1>Hi! My Name is Jose Cabrera</h1>
+    <h2>and i'm the <span> Software Developer </span></h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from "vue";
+export default defineComponent({
+  name: "HelloWorld",
+  props: {
+    msg: {
+      type: String,
+      required: true,
+      default: "",
+    },
+  },
+  setup: () => {
+    const count = ref(0);
+    return { count };
+  },
+});
+</script>
+
+<style scoped>
+#container {
+  height: 90vh;
+  border-style: solid;
+  border-color: red;
+  border-width: 2px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+span {
+  font-weight: bolder;
+}
+
+h1,
+h2 {
+  border-style: solid;
+  border-color: red;
+  border-width: 2px;
+}
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
