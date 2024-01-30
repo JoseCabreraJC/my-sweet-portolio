776ae871ebe9cbb94fac21fd0bc6351244944d70
Jose Cabrera <josecabrera@cloud.upwork.com>
2024-01-30 12:27:16 +1100

    added router dom and initial routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
-    </div>
+    <main className='bg-slate-300/20'>
+      <Router>
+        Navbar
+        <Routes>
+          <Route path='/' element={'Home'} />
+          <Route path='/about' element={'About'} />
+          <Route path='/projects' element={'Project'} />
+          <Route path='/contact' element={'Contact'} />
+        </Routes>
+      </Router>
+    </main>
   )
 }
 
